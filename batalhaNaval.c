95ba18dee06ad691585329f5488463b8d184cdd0
evandrolopes85 <evandrocwd@gmail.com>
2025-03-24 22:42:08 -0300

    Desafio novato concluido, exibindo navios nas posicoes horizontais e verticais

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,6 +4,78 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
+void preencherTabuleiro(int tabuleiro[10][10], int tamanhoLinha, int tamanhoColuna){
+    for(int i = 0; i < tamanhoLinha; i++){
+        for(int j = 0; j < tamanhoColuna; j++){
+            tabuleiro[i][j] = 0;
+        }
+    }
+}
+
+void exibirTabuleiro(int tabuleiro[10][10], char colunasTabuleiro[10], int tamanhoLinha, int tamanhoColuna){
+    for(int i = 0; i < tamanhoLinha; i++){
+        if(i == 0)
+            printf("%5c ", colunasTabuleiro[i]);
+        else
+            printf("%2c ", colunasTabuleiro[i]);
+    }
+    printf("\n");
+    for(int i = 0; i < tamanhoLinha; i++){
+        printf("%2d ", i+1);
+        for(int j = 0; j < tamanhoColuna; j++){
+            printf("%2d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int navioVertical(int tabuleiro[10][10], int navio[3], int tamanhoLinha, int tamanhoColuna, int coordenadaX, int coordenadaY){
+    if(coordenadaX > 7){
+        printf("Coordenada informada ultrapassam o tamanho do tabuleiro. Tente novamete!\n");
+        return 1;
+    }else if (coordenadaY > 7){
+        printf("Coordenada informada ultrapassam o tamanho do tabuleiro. Tente novamete!\n");
+        return 1;
+    }
+    int index = 0;
+    for(int i = index; i < tamanhoLinha; i++){
+        if(tabuleiro[i + coordenadaX][coordenadaY] != 0){
+            printf("Posição x[%d] e y[%d] ocupada!\n", i + coordenadaX, coordenadaY);
+            return 1;
+        }
+        tabuleiro[i + coordenadaX][coordenadaY] = navio[index];
+        index++;
+        if(index == 3){
+            break;
+        }
+    }
+
+    return 0;
+}
+
+int navioHorizontal(int tabuleiro[10][10], int navio[3], int tamanhoLinha, int tamanhoColuna, int coordenadaX, int coordenadaY){
+    if(coordenadaX > 7){
+        printf("Coordenada informada ultrapassam o tamanho do tabuleiro. Tente novamete!\n");
+        return 1;
+    }else if (coordenadaY > 7){
+        printf("Coordenada informada ultrapassam o tamanho do tabuleiro. Tente novamete!\n");
+        return 1;
+    }
+    int index = 0;
+    for(int j = index; j < tamanhoColuna; j++){
+        if(tabuleiro[coordenadaX][j + coordenadaY] != 0){
+            printf("Posição x[%d] e y[%d] ocupada!\n", coordenadaX, j + coordenadaY);
+            return 1;
+        }
+        tabuleiro[coordenadaX][j + coordenadaY] = navio[index];
+        index++;
+        if(index == 3){
+            break;
+        }
+    }
+    return 0;
+}
+
 int main() {
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
@@ -14,28 +86,26 @@ int main() {
     int tabuleiro[LINHA][COLUNA];
     int navio1[3] = {3,3,3};
     int navio2[3] = {3,3,3};
+    int coordenadaX = 3;
+    int coordenadaY = 3;
 
-    for(int i = 0; i < LINHA; i++){
-        for(int j = 0; j < COLUNA; j++){
-            tabuleiro[i][j] = 0;
-        }
-    }
+    preencherTabuleiro(tabuleiro, LINHA, COLUNA);
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
+    coordenadaX--;  // subtrai coordenada tendo em vista que o tabuleiro começa na posicao 0 e não 1
+    coordenadaY--;  // subtrai coordenada tendo em vista que o tabuleiro começa na posicao 0 e não 1
+
+    navioVertical(tabuleiro, navio1, LINHA, COLUNA, coordenadaX, coordenadaY);
+
+    coordenadaX = 8; // Se coordenada maior que 8 vair retornar erro, ultrapassa o tamanho do tabuleiro
+    coordenadaY = 8; // Se coordenada maior que 8 vair retornar erro, ultrapassa o tamanho do tabuleiro
+    coordenadaX--; // subtrai coordenada tendo em vista que o tabuleiro começa na posicao 0 e não 1
+    coordenadaY--; // subtrai coordenada tendo em vista que o tabuleiro começa na posicao 0 e não 1
+    int isErro = navioHorizontal(tabuleiro, navio2, LINHA, COLUNA, coordenadaX, coordenadaY); 
+
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-    for(int i = 0; i < LINHA; i++){
-        if(i == 0)
-            printf("%5c ", colunasTabuleiro[i]);
-        else
-            printf("%2c ", colunasTabuleiro[i]);
-    }
-    printf("\n");
-    for(int i = 0; i < LINHA; i++){
-        printf("%2d ", i+1);
-        for(int j = 0; j < COLUNA; j++){
-            printf("%2d ", tabuleiro[i][j]);
-        }
-        printf("\n");
-    }
+    if(isErro == 0)
+        exibirTabuleiro(tabuleiro, colunasTabuleiro, LINHA, COLUNA);
+    
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
